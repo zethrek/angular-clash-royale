0ce1047754467ff1b95631a9461e2363d78ff6bd
Gabriel Jiménez <gabriel.jimenez@zeke.cl>
2018-12-27 12:46:34 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-clash-royale';
+
+export class AppComponent implements OnInit {
+  title = 'app';
+  restItems: any;
+  restItemsUrl = 'http://localhost:8000/api/players';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getRestItems();
+  }
+
+  // Read all REST Items
+  // Todo: Obtener resultados por Tag del jugador y desplegarlos formateados en la vista. Actualmente solo se obtiene en consola.
+  // Todo: Modularizar Component y Service "Player".
+  getRestItems(): void {
+    this.restItemsServiceGetRestItems()
+      .subscribe(
+        restItems => {
+          this.restItems = restItems;
+          console.log(this.restItems);
+        }
+      );
+  }
+
+  // Rest Items Service: Read all REST Items
+  restItemsServiceGetRestItems() {
+    return this.http
+      .get<any[]>(this.restItemsUrl)
+      .pipe(map(data => data));
+  }
 }
